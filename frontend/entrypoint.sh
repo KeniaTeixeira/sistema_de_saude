563fa9e77d6d611285da3482e4495f31c10cd9ae
Ana Clara Duarte <anaclaraduarte0011@gmail.com>
2026-06-11 20:53:00 -0300

    Fix deploy: entrypoint nginx + dockerContext no render.yaml

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+BACKEND_URL=${BACKEND_URL:-http://backend:8080}
+export BACKEND_URL
+envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
