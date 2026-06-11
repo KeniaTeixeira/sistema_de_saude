8892b71f1efa8e07d655cb104d15c348b6a9e929
Ana Clara Duarte <anaclaraduarte0011@gmail.com>
2026-06-11 20:40:03 -0300

    Deploy Inicial Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
-
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: '/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
